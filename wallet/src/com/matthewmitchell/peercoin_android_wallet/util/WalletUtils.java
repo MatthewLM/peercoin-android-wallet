05b3b15349eaaa09952ff3eae4c26389523ab6c4
Matthew Mitchell <matthewmitchell@thelibertyportal.com>
2015-01-27 18:03:43 +0000

    Undo change to Bitcoin by accident

--- wallet/src/com/matthewmitchell/peercoin_android_wallet/util/WalletUtils.java
+++ wallet/src/com/matthewmitchell/peercoin_android_wallet/util/WalletUtils.java
@@ -210,7 +210,7 @@ public class WalletUtils
 	{
 		final DateFormat format = Iso8601Format.newDateTimeFormatT();
 
-		out.write("# KEEP YOUR PRIVATE KEYS SAFE! Anyone who can read this can spend your Bitcoins.\n");
+		out.write("# KEEP YOUR PRIVATE KEYS SAFE! Anyone who can read this can spend your Peercoins.\n");
 
 		for (final ECKey key : keys)
 		{
